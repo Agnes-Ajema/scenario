eaf560809acd7d190334baa10d2e4eed6a418322
Agnes <agnesajema@gmail.com>
2024-05-09 22:58:51 +0300

    add changes

--- app.py
+++ app.py
@@ -20,3 +20,15 @@ user_app.add_user("Ajema")
 
 print(f"User {Ajema.username}has ID {Ajema_id}")
 print(f"User {Agnes.username} has ID {Agnes_id}")
+
+user_followers =[
+    {"username": "user1","followers":["follower1","follower2","follower3"]},
+    {"username": "user2","followers":["follower5","follower6","follower7"]}
+]
+def get_followers(username):
+    for user in user_followers:
+        if user["username"] == username:
+            return user["followers"]
+    return[]
+print(get_followers("user1"))
+print(get_followers("user3"))
